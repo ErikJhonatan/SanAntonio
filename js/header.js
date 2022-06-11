24d0cb913518ed2f4e7e96e26c0865dfe2f1d610
ErikJhonatan <marcaloayzae@gmail.com>
2022-06-10 22:48:33 -0500

    "Cambios importantes en el sitio web"

--- js/header.js
+++ js/header.js
@@ -8,3 +8,14 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+//header sticky
+const headerContainer = document.querySelector(".header-container");
+
+  window.addEventListener("scroll", function() {
+    if (window.pageYOffset > 0) {
+      headerContainer.classList.add("header-container_scrolled");
+    } else {
+      headerContainer.classList.remove("header-container_scrolled");
+    }
+  });
