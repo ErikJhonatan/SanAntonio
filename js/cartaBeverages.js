cff79a14816c20c7bcd69a63cfc8ac16e55ac04f
ErikJhonatan <marcaloayzae@gmail.com>
2022-06-29 13:29:59 -0500

    "Cambio de documnet.title = bebidas "

--- js/cartaBeverages.js
+++ js/cartaBeverages.js
@@ -3,7 +3,7 @@ const btnBeveragesClose = document.querySelector('.beverages__close');
 function toggleBeverages() {
     const beverages = document.querySelector('.beverages');
     beverages.classList.toggle('beverages_opened');
+    window.document.title = 'Bebidas | San Antonio';
 }
     btnBeverages.addEventListener('click', toggleBeverages);
     btnBeveragesClose.addEventListener('click', toggleBeverages);
-    
