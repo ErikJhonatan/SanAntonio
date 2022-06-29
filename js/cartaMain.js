7fe286cb90790a05d981cbf04c62c0bcd3791060
ErikJhonatan <marcaloayzae@gmail.com>
2022-06-29 09:47:55 -0500

    Cambios importantes

---
+++ js/cartaMain.js
@@ -0,0 +1,9 @@
+const btnParrilla = document.querySelector(".btn-parrilla");
+const btnBrasa = document.querySelector(".btn-brasa");
+const fastFood = document.querySelector(".fast-food");
+const btnSoup = document.querySelector(".btn-soup");
+const btnBeverages = document.querySelector(".btn-beverages");
+
+
+
+
